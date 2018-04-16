77a087bd91776beb897a5fcaae7a43c4f6cb6d01
池田雄太郎 <ikedayutaro@ikedayutaro-mac.local>
2018-04-16 10:28:23 +0900

    add markdowns

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -12,28 +12,6 @@
 </template>
 
 <script>
-const api = "https://api.github.com/repos/ikedaosushi/tech-news/issues"
-import _ from 'lodash'
-
-const decodeEntities = (function() {
-  // this prevents any overhead from creating the object each time
-  let element = document.createElement('div');
-
-  function decodeHTMLEntities (str) {
-    if(str && typeof str === 'string') {
-      // strip script/html tags
-      str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
-      str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
-      element.innerHTML = str;
-      str = element.textContent;
-      element.textContent = '';
-    }
-
-    return str;
-  }
-
-  return decodeHTMLEntities;
-})();
 
 export default {
   components: { },
@@ -43,17 +21,6 @@ export default {
     }
   },
   async created() {
-    const resp = await this.$axios.get(api)
-    this.newses = resp.data
-    this.newses.forEach(news => {
-      let bodies = decodeEntities(news.body).split("\n") // 3つに分かれる
-      if (news.title === bodies[0].replace(/[、【】]/gi, "")) { // 1つ目の要素が同じ場合がある
-        bodies.shift()
-      }
-      const url = bodies.pop()
-      news.url = url
-      news.desc = bodies.join("\r\r").replace("　", "").replace(" ", "").replace("　", "")
-    })
   },
   methods: {
   }
